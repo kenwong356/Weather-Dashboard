035d504f2f67fc366bf32d04fd0b902f99a7a97f
Ken <kanney356@gmail.com>
2023-11-09 23:27:53 -0800

    added second api link for 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
         method: "GET",
           url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey,
     
-    
+
         }).then(function (data) {
           $("#today").empty();
     
@@ -70,6 +70,39 @@ $(document).ready(function () {
           card.append(cardBody);
           $("#today").append(card);
           console.log(data);
+        });
+    }
+    
+    function weatherForecast(searchTerm) {
+        $.ajax({
+        method: "GET",
+          url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=" + apiKey + "&units=imperial",
+        }).then(function (data) {
+          console.log(data);
+          $("#forecast").html("<div class=\"forecast-container\">");
+      
+          var heading = $("<h2>").text("5 Day Forecast:");
+          $("#forecast .forecast-container").append(heading);
+          var cardRow = $("<div>").addClass("row");
+      
+          for (var i = 0; i < data.list.length; i++) {
+              if (new Date(data.list[i].dt_txt).getHours() === 15) {
+    
+              var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+              var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+              var colFive = $("<div>").addClass("col-md-2.5");
+              var cardFive = $("<div>").addClass("card bg-primary text-white");
+              var cardBodyFive = $("<div>").addClass("card-body p-2");
+              var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+              var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+              var windSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + data.list[i].wind.speed + " mph");
+      
+              colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, tempFive, windSpeed, humidFive)));
+              cardRow.append(colFive);
+            }
+          }
+      
+          $("#forecast .forecast-container").append(cardRow);  
         });
 }
 });
